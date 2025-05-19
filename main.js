e2652f86b107f23b2e56674479f94b2b6aa47605
Zerth1 <ryanbillah@gmail.com>
2025-05-19 19:12:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1744,6 +1744,7 @@ let wrongCorner = 0;
 let wrongSound = 0;
 let wrongColor = 0;
 
+loadBindings();
 // Events
 [ ...document.querySelectorAll("input[name='dimension'") ].forEach(el => {
   el.addEventListener("click", function(evt) {
@@ -1781,7 +1782,6 @@ document.addEventListener("keydown", evt  => {
   }
 });
 
-loadBindings();
 loadSettings();
 loadHistory();
 console.log("Running")
